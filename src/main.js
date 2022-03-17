e1470668fcdd22118ea4233b05d56ab1c471766c
ahmetberke <tuncel.ahmet.berke@gmail.com>
2022-03-18 00:07:17 +0300

    included bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vue = createApp(App)
+vue.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
