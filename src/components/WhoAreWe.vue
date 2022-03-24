cd4320fb2abbea8e4c7444671ad362ea8806aa56
Cagriucaroglu <aucaroglu7075@gmail.com>
2022-03-24 15:34:27 +0300

    created who are we and added data

---
+++ src/components/WhoAreWe.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "BİZ KİMİZ?",
+      explanation: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      managers: [
+        {name: "Enes Batur", picture: "person.jpg", title: "Yönetim Kurulu Eş Başkanı", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
+        {name: "Çağrı Uçmazkızı", picture: "person.jpg", title: "Yönetim Kurulu Eş Başkanı", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
+      ],
+      team: [
+        {name: "Selin Toplayıcı", picture: "person.jpg"},
+        {name: "Ahmet Berke Demirayak", picture: "person.jpg"},
+        {name: "Cem koşmuş", picture: "person.jpg"},
+        {name: "elif kaplan", picture: "person.jpg"},
+      ]
+    }
+  }
+}
+</script>
